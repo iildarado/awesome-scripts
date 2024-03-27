642967a621430369d6d3ff7b0bcdf60f69faa385
Almas Sinev <iildarado26@gmail.com>
2024-03-27 18:36:10 +0300

    init

---
+++ c/cube.c
@@ -0,0 +1,8 @@
+#include <math.h>
+
+float A, B, C;
+
+float calculateX(int i, int j, int k) {
+    return j*sin(A)*sin(B)*cos(C) - k*cos(A)*sin(B)*cos(C) + 
+            j*cos(A)*sin(C) + k*sin(A)*sin(C) + i*cos(B)*cos(C);
+}
